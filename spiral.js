9c5a166599f83654eef18390c64d132e3c62bc71
julindramarkus <julindramarkus11@gmail.com>
2017-09-06 07:38:52 +0700

    selesai

--- spiral.js
+++ spiral.js
@@ -1,52 +1,58 @@
+"use strict"
+
 function spiral(n) {
   let count = 0, grid = new Array(n);
-  for(i=0; i<n; i++) {
+  let result = [];
 
-  grid[i] = new Array(n);
-  for(j=0; j<n; j++){
-    grid[i][j] = count;
-    count++;
+  for(let i=0; i<n; i++) {
+    grid[i] = new Array(n);
+    for(let j=0; j<n; j++){
+      grid[i][j] = count;
+      count++;
+    }
   }
-}
 
-let top = 0, left = 0, bottom = n, right = n;
-let direction = "right";
-while(top<=bottom && left<=bottom)
-{
-  if (direction =="right")
+  let top = 0, left = 0, bottom = n, right = n;
+  let direction = "right";
+  while(top<=bottom && left<=bottom)
   {
+    if (direction =="right")
+    {
       for (let i=left; i<right; i++)
       {
-          console.log(grid[top][i])
+        result.push(grid[top][i])
       }
       top= top+1;
       direction = "bottom";
-  }    else if (direction =="bottom")
-  {
+    }    else if (direction =="bottom")
+    {
       for (let i=top; i<bottom; i++)
       {
-          console.log(grid[i][right-1])
+        result.push(grid[i][right-1])
       }
       right= right-1;
       direction = "left";
-  }    else if (direction =="left")
-  {
+    }    else if (direction =="left")
+    {
       for (let i=right-1; i>=left; i--)
       {
-          console.log(grid[bottom-1][i])
+        result.push(grid[bottom-1][i])
       }
       bottom = bottom-1;
       direction = "top";
-  }    else if (direction =="top")
-  {
+    }    else if (direction =="top")
+    {
       for (let i=bottom-1; i>=top; i--)
       {
-          console.log(grid[i][left])
+        result.push(grid[i][left])
       }
       left= left+1;
       direction = "right";
+    }
   }
-}
+  console.log(result);
 }
 
 spiral(5);
+spiral(6);
+spiral(7);
